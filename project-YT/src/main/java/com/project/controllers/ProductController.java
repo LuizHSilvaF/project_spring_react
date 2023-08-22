25ac8fde0e7cd0b2e68ba605ad1633d3b37f35bb
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-08-22 13:37:42 -0300

    Adding data

--- project-YT/src/main/java/com/project/controllers/ProductController.java
+++ project-YT/src/main/java/com/project/controllers/ProductController.java
@@ -3,7 +3,12 @@ package com.project.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,15 @@ public class ProductController {
 	@GetMapping
 	public List<Product> findAll(){
 	return service.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> addProduct(@RequestBody Product product) {
+		return ResponseEntity.ok(service.addProduct(product));
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product){
+		return ResponseEntity.ok(service.update(id, product)).getBody();
 	}
 }
--- project-YT/src/main/java/com/project/services/ProductService.java
+++ project-YT/src/main/java/com/project/services/ProductService.java
@@ -3,8 +3,11 @@ package com.project.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.project.entities.AnswerModel;
 import com.project.entities.Product;
 import com.project.repositories.ProductRepository;
 
@@ -14,7 +17,38 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repo;
 	
+	@Autowired
+	private AnswerModel answer;
+	
 	public List<Product> findAll(){
 		return repo.findAll();
 	}
+	
+	public ResponseEntity<?> addProduct(Product product) {
+		if(product.getName().equals("")) {
+			answer.setMsg("O nome do produto é obrigatório.");
+			return new ResponseEntity<AnswerModel>(answer, HttpStatus.BAD_REQUEST);
+		}
+		else if(product.getBrand().equals("")) {
+			answer.setMsg("O nome da marca é obrigatório.");
+			return new ResponseEntity<AnswerModel>(answer, HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<Product>(repo.save(product), HttpStatus.CREATED);
+		}
+
+	
+	public ResponseEntity<?> update(Long id, Product product){
+		if(product.getName().equals("")) {
+			answer.setMsg("O nome do produto é obrigatório.");
+			return new ResponseEntity<AnswerModel>(answer, HttpStatus.BAD_REQUEST);
+		}
+		else if(product.getBrand().equals("")) {
+			answer.setMsg("O nome da marca é obrigatório.");
+			return new ResponseEntity<AnswerModel>(answer, HttpStatus.BAD_REQUEST);
+		}
+		product.setId(id);
+		return new ResponseEntity<Product>(repo.save(product), HttpStatus.CREATED);
+		}
+
 }
+
