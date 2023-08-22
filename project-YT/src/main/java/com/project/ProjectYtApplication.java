f380fd516d04e67bbcac7ba720d256231f97e8cc
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-08-21 23:32:58 -0300

    Project spring_react created

---
+++ project-YT/src/main/java/com/project/ProjectYtApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectYtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectYtApplication.class, args);
+	}
+
+}
---
+++ project-YT/src/main/java/com/project/entities/Product.java
@@ -0,0 +1,71 @@
+package com.project.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String brand;
+	
+	public Product() {
+	}
+
+	public Product(Long id, String name, String brand) {
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+		
+}
